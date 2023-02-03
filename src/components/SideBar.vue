c37c476964db40ed235a2434fa10d72522dc9eff
virginie <vvillard@antidots-group.com>
2023-02-03 16:07:18 +0100

    Update SideBar, add logo.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="sidebar">
-
+        <div id="logo">
+            <img src="https://www.ubongo.org/wp-content/uploads/Pineapple-Logo-transparent-BG.png" alt="Pineapple-TV-Logo">
+        </div>
     </div>
 </template>
 
@@ -20,4 +22,23 @@ export default {
     left: 0px;
 }
 
+#logo>img {
+    width: 140px;
+    padding-top: 16px;
+}
+
 </style>
+
+
+<!--display: flex;-->
+<!--flex-direction: column;-->
+<!--justify-content: center;-->
+<!--align-items: center;-->
+<!--padding: 0px;-->
+<!--gap: 50px;-->
+
+<!--position: absolute;-->
+<!--width: 30px;-->
+<!--height: 430px;-->
+<!--left: 72px;-->
+<!--top: 437px;-->
