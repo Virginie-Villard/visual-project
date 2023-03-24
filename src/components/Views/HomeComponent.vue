2cd9aa1b59f1a0ebfb2feeb93f0b182bac444be6
virginie <vvillard@antidots-group.com>
2023-03-24 14:57:19 +0100

    Add class content & EventComponent (footer) & image style to HomeComponent.vue

--- src/components/Views/HomeComponent.vue
+++ src/components/Views/HomeComponent.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="content">
+        <h1>Enjoy<br>Anywhere</h1>
+
+        <img src="../../assets/images/child-with-a-backpack.png" alt="child with a backpack and a yellow coat">
+
+        <event-component></event-component>
     </div>
 </template>
 
 <script>
+import EventComponent from "../EventComponent.vue";
+
 export default {
-    name: "HomeComponent"
+    name: "HomeComponent",
+
+    components: {
+        EventComponent
+    }
 }
 </script>
 
 <style scoped>
+
+img {
+    position: absolute;
+    width: 494px;
+    height: 928px;
+    left: 518px;
+    top: 71px;
+    z-index: 1;
+}
 
 </style>
